ed0e9544b5c853dc060e5551072ddd6b26281210
Rahmaahmed685 <rahmaahmed8276@email.com>
2024-03-04 14:32:21 +0200

    responsive_sizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:graduationproject/screens/welcome_screen.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -11,15 +17,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const WelcomeScreen()
+    return ResponsiveSizer(
+        builder: (context, orientation, screenType) {
+          return
+            MaterialApp(
+                locale: DevicePreview.locale(context),
+                builder: DevicePreview.appBuilder,
+                title: 'Flutter Demo',
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                  colorScheme: ColorScheme.fromSeed(
+                      seedColor: Colors.deepPurple),
+                  useMaterial3: true,
+                ),
+                home: const WelcomeScreen()
+            );
+        }
     );
   }
 }
-
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -16,37 +17,42 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
        Column(
          mainAxisAlignment: MainAxisAlignment.spaceBetween,
          children: [
-           SizedBox(height: 70,),
+           SizedBox(height: 40.sp,),
          Expanded(
            child:
            Row(
                crossAxisAlignment: CrossAxisAlignment.start,
-       children:[
-             Image.asset("assets/images/image 1.png"),
-         Container(
-           height: 150,
-           width: 150,
-           child:
-           Image.asset("assets/images/image 2.png"),
+               children:[
+                 Expanded(child: Image.asset("assets/images/image 1.png")),
+                 Expanded(
+                   child: Container(
+                     height: 50.sp,
+                     width: 50.sp,
+                     child:
+                     Image.asset("assets/images/image 2.png"),
+                   ),
+                 ),
+               ]),
          ),
-           ]),
-         ),
-         Image.asset('assets/images/Desk Share-logos_transparent 1.png'),
-        SizedBox(height: 60,),
-         Expanded(
-           child: Row(
-   crossAxisAlignment: CrossAxisAlignment.start,
-             children: [
-             Image.asset('assets/images/image 3.png'),
-           Container(
-             height: 230,
-             width: 215,
-             child:
-               Image.asset('assets/images/image 4.png'),
+           SizedBox(height: 30.sp,),
+           Image.asset('assets/images/Desk Share-logos_transparent 1.png'),
+           SizedBox(height: 30.sp,),
+           Expanded(
+             child: Row(
+               crossAxisAlignment: CrossAxisAlignment.start,
+               children: [
+                 Expanded(child: Image.asset('assets/images/image 3.png')),
+                 Expanded(
+                   child: Container(
+                     height: 80.sp,
+                     width: 60.sp,
+                     child:
+                     Image.asset('assets/images/image 4.png'),
+                  ),
+                 ),
+               ],),
            ),
-           ],),
-         )
-
+           SizedBox(height: 30.sp,),
        ],)
     );
   }
