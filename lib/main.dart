4f9e6260a7f193208a2caa476245b646fc3a8122
AhmedAlraey <120908760+AhmedAlraey@users.noreply.github.com>
2024-04-04 19:38:03 +0200

    ahmed

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graduationproject/colors/colors.dart';
 import 'package:graduationproject/main_page.dart';
 import 'package:graduationproject/screens/authentication/login_screen.dart';
+import 'package:graduationproject/screens/nav_bar_screens/home_screen.dart';
 import 'package:graduationproject/screens/nav_bar_screens/profile_screen.dart';
 import 'package:graduationproject/screens/sub_pages/edit_screen.dart';
 import 'package:graduationproject/screens/sub_pages/notifications.dart';
@@ -15,74 +16,59 @@ import 'package:responsive_sizer/responsive_sizer.dart';
 import 'core/auth_cubit.dart';
 
 void main() {
- runApp(
-  DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => MyApp(), // Wrap your app
-  ),
-);
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(), // Wrap your app
+    ),
+  );
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MultiBlocProvider(
+    return MultiBlocProvider(
         providers: [
           BlocProvider(
             create: (context) => AuthCubit(),
           ),
         ],
-        child: BlocBuilder<AuthCubit, AuthState>(
-        builder: (context, state) {
-      return
-      ResponsiveSizer(
-        builder: (context, orientation, screenType) {
-          return
-            MaterialApp(
+        child: BlocBuilder<AuthCubit, AuthState>(builder: (context, state) {
+          return ResponsiveSizer(builder: (context, orientation, screenType) {
+            return MaterialApp(
                 locale: DevicePreview.locale(context),
                 builder: DevicePreview.appBuilder,
                 title: 'Flutter Demo',
                 debugShowCheckedModeBanner: false,
-              theme:
-              ThemeData(
-                  brightness: Brightness.light,
-                  useMaterial3: true,
-                  appBarTheme: AppBarTheme(
-                      color: Colors.white,
-                      titleTextStyle: TextStyle(
+                theme: ThemeData(
+                    brightness: Brightness.light,
+                    useMaterial3: true,
+                    appBarTheme: AppBarTheme(
+                        color: Colors.white,
+                        titleTextStyle:
+                            TextStyle(color: Colors.black87, fontSize: 20),
+                        iconTheme: IconThemeData(color: Colors.black87)),
+                    scaffoldBackgroundColor: Colors.white,
+                    textTheme: TextTheme(
+                        titleSmall: TextStyle(
                           color: Colors.black87,
-                          fontSize: 20
-                      ),
-                      iconTheme: IconThemeData(color: Colors.black87)
-                  ),
-                  scaffoldBackgroundColor: Colors.white,
-                  textTheme: TextTheme(
-                      titleSmall: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 15,
-                      ),
-                      titleMedium: TextStyle(
-                        color: Colors.black87,
-                        fontSize: 20,
-                        fontWeight: FontWeight.w500,
-                      ),
-                      titleLarge: TextStyle(
+                          fontSize: 15,
+                        ),
+                        titleMedium: TextStyle(
                           color: Colors.black87,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold
-                      )
-                  ),
-                  iconTheme: IconThemeData(color: Colors.black87)
-
-              ),
-                home: SelectLanguageScreen()
-            );
-        }
-    );
-        })
-    );
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        titleLarge: TextStyle(
+                            color: Colors.black87,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold)),
+                    iconTheme: IconThemeData(color: Colors.black87)),
+                home: HomeScreen());
+          });
+        }));
   }
 }
-
--- lib/screens/nav_bar_screens/home_screen.dart
+++ lib/screens/nav_bar_screens/home_screen.dart
@@ -12,10 +12,199 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home Screen",style: TextStyle(color: Colors.white),)
-        ,backgroundColor: mainColor,
-      ),
+      body: Container(
+        margin: EdgeInsets.all(25),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //1**********************************
+            Row(children: [
+              Icon(Icons.location_on_outlined),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Current location"),
+                  Text(
+                    "Kuala Lumpur,Malaysia",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                width: 220,
+              ),
+              Icon(
+                Icons.notifications_none,
+                size: 30,
+              )
+            ])
 
+            ///2******************************************************************** */
+            ,
+            TextField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                  ),
+                  border: InputBorder.none,
+                  hintText: "  Where do you want yo work?",
+                  hintStyle: TextStyle(height: 5)),
+            ),
+
+            ///3************************************************************************ */
+
+            Text(
+              "  Only for Today",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+            //4************************************************************************* */
+
+            Stack(children: [
+              Image.asset(
+                  "FinalProject/assets/images/photo_2024-04-04_12-18-30.jpg",
+                  height: 170,
+                  width: 500,
+                  fit: BoxFit.cover),
+              Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("30% Off",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40)),
+                      Text("All Food & Beverage",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          )),
+                      SizedBox(
+                        height: 25,
+                      ),
+                      Text("By Only book for 1 hour",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25)),
+                    ],
+                  ))
+            ]),
+            //5********************************************************************
+
+            Text("Sponsored by Some random place"),
+            SizedBox(
+              height: 20,
+            ),
+            //6********************************************************* */
+
+            Row(
+              children: [
+                Text(
+                  "Popular Workspace",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                SizedBox(
+                  width: 120,
+                ),
+                Text("See all")
+              ],
+            ),
+
+            //7********************************************************
+
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Image.asset(
+                      "FinalProject/assets/images/photo_2024-04-04_12-18-30.jpg",
+                      height: 200,
+                      width: 200,
+                    ),
+                    Container(
+                      child: Icon(
+                        Icons.favorite,
+                        size: 30,
+                        color: Color.fromARGB(255, 0, 85, 255),
+                      ),
+                      padding: EdgeInsetsDirectional.only(top: 50, start: 20),
+                    )
+                  ],
+                ),
+                ///////////////////////
+                Container(
+                  padding: EdgeInsetsDirectional.only(top: 50),
+                  width: 250,
+                  height: 200,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Gowork park 29",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                        Row(children: [
+                          Icon(Icons.location_on_outlined),
+                          Text("petaling Malaysia")
+                        ]),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.grade,
+                              color: Color.fromARGB(255, 24, 42, 238),
+                            ),
+                            Icon(
+                              Icons.grade,
+                              color: Color.fromARGB(255, 24, 42, 238),
+                            ),
+                            Icon(
+                              Icons.grade,
+                              color: Color.fromARGB(255, 24, 42, 238),
+                            ),
+                            Icon(
+                              Icons.grade,
+                              color: Color.fromARGB(255, 24, 42, 238),
+                            ),
+                            Icon(
+                              Icons.grade,
+                              color: Color.fromARGB(255, 24, 42, 238),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "&17",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                            Text("/Hour"),
+                            SizedBox(width: 120),
+                            Text(
+                              "&*",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20),
+                            ),
+                          ],
+                        )
+                      ]),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
