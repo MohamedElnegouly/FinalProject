b3566533e36e911a5f8ba44f0a39a42af1957f39
Rahmaahmed685 <rahmaahmed8276@email.com>
2024-05-23 19:33:00 +0300

    update

--- lib/view/search/repos/search_repo.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:graduationproject/view/search/search_model.dart';
-
-abstract class SearchRepo {
-  Future<Either<String, List<SearchModel>>> searchRepo();
-}
