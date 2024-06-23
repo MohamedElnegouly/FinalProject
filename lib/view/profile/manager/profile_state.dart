083607cc00e93faf0a6d3230eff3332f34f701f4
Rahmaahmed685 <rahmaahmed8276@email.com>
2024-06-23 22:08:10 +0300

    last

--- lib/view/profile/manager/profile_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'profile_cubit.dart';
-
-@immutable
-abstract class ProfileState {}
-
-class ProfileInitial extends ProfileState {}
-class ProfileLoading extends ProfileState {}
-class GetProfileSuccess extends ProfileState {}
-class FailedToGetProfile extends ProfileState {}
